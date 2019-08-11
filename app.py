9935f4febd39fb516cbe834f49948b75f1c2d140
Danny Aziz <daanyaal997@gmail.com>
2019-08-11 14:36:36 +0100

    Return first page IDS

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-from chalice import Chalice
+from chalice import Chalice, Response
+from requests_html import HTMLSession
 
 app = Chalice(app_name='twitter_interactions')
 
+twitter_url = 'https://mobile.twitter.com/search?q=from%3A{}%20to%3A{}'
 
-@app.route('/')
+
+@app.route('/fetch-interactions')
 def index():
-    return {'hello': 'world'}
+    request = app.current_request
+    if "from_user" not in request.query_params:
+        return Response(
+            body={"error": "No from_user supplied"},
+            status_code=400
+        )
+    if "to_user" not in request.query_params:
+        return Response(
+            body={"error": "No to_user supplied"},
+            status_code=400
+        )
 
+    url = twitter_url.format(request.query_params["from_user"], request.query_params["to_user"])
+    session = HTMLSession()
+    r = session.get(
+        url,
+        headers={
+            'cookie': 'm5=off'
+        }
+    )
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    ids = []
+    timeline = r.html.find(".timeline", first=True)
+    if timeline:
+        tweets = timeline.find("table")
+        for tweet in tweets:
+            ids.append(
+                tweet.find(".timestamp", first=True).find("a", first=True).attrs["name"].replace("tweet_", "")
+            )
+        return ids
+    return {"error": True}
